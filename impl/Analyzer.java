358f251112d9e4fcff72a3220a9ccd86c4ba1cb5
janhendrikstruve <janhendrikstruve@gmail.com>
2023-12-18 18:46:26 +0100

    Finish Analyser

--- impl/Analyzer.java
+++ impl/Analyzer.java
@@ -1,9 +1,5 @@
 package de.hawhamburg.hamann.ad.trees.impl;
 
-import de.hawhamburg.hamann.ad.trees.BinarySearchTree;
-
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,6 +17,8 @@ public class Analyzer {
     static public void analyzer () throws IOException {
         List<Integer> uniqueRandomNumbers = new ArrayList<Integer>();
         BSTree<Integer, Integer> bsTree; // Anstatt BinarySearchTree<Integer, Integer> bsTree;
+        List<Integer> measurements = new ArrayList<>();
+        int measureAverage = 0;
 
         final String csvPath = "plots/averagePathLength.csv";
 
@@ -28,21 +26,34 @@ public class Analyzer {
 
             writer.write("numberOfKeys, averagePathLength" + "\n");
 
-            for (int q = 0; q < 100; q++) {
 
-                bsTree = new BSTree<Integer, Integer>();
+            for (int n = 100; n <= 10_000; n += 100) {
 
+                System.out.println(n/100);
+                measurements.clear();
                 for (int i = 0; i < 1000; i++) {
+                    bsTree = new BSTree<Integer, Integer>();
+                    uniqueRandomNumbers.clear();
 
-                    uniqueRandomNumbers.add(i);
+
+                    for (int r = 0; r < n; r++) {
+
+                        uniqueRandomNumbers.add(r);
+                    }
+                    Collections.shuffle(uniqueRandomNumbers);
+                    for (int randomNumber : uniqueRandomNumbers) {
+
+                        bsTree.insert(randomNumber, randomNumber);
+                    }
+
+                    measurements.add(bsTree.averagePathLength());
                 }
-                Collections.shuffle(uniqueRandomNumbers);
-                for (int i = 0; i < 1000; i++) {
 
-                    bsTree.insert(i, i);
+                for (int measurement : measurements) {
+                    measureAverage += measurement;
                 }
-
-                writer.write(q*100 + "," + bsTree.averagePathLength() + "\n");
+                measureAverage /= measurements.size();
+                writer.write(n + "," + measureAverage + "\n");
             }
         }
     }
